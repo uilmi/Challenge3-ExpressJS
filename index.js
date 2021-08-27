16f11b7a9005bb69c4164a49c30397ee8f708ce9
uilmi <uilmi@na.edu>
2021-08-27 22:02:30 +0700

    view engine is ready to rock

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        linkGame: "game"
+    })
+})
+
+app.get('/game', (req, res) => {
+    res.render('game', {
+        back: "/",
+        linkGame: "game"
+    })
+})
+
 
 app.listen(port, () => {
-    console.log(`The app is running on port ${port}`);
+    console.log(`The app is running on ${port}`);
 });
